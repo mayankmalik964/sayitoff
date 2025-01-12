6a5298f06b559d59b7a614e418f03cb6623ec30a
mayank <mayankmalik964@gmail.com>
2025-01-12 17:10:50 +0530

    initial commit

---
+++ sayitoff1/app.js
@@ -0,0 +1,67 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:"true"}));
+app.set(path.join(__dirname,"views"));
+let port=8080;
+const { v4: uuidv4 } = require('uuid');
+const methodoverride=require("method-override");
+app.use(methodoverride("_method"));
+app.listen(port,()=>{
+    console.log("listening");
+})
+let posts=[{
+    id:"1a",
+    name:"mayank",
+    comment:"doing web devlopement"
+}
+,{id:"1b",
+    name:"vijay",
+    comment:"doing competitive programming"
+},
+{   id:"1c",
+    name:"harsh",
+    comment:"doing everything"
+}]
+app.get("/quora",(req,res)=>{
+    res.render("index.ejs" ,{posts:posts});
+})
+app.get("/quora/new",(req,res)=>{
+     res.render("new.ejs");
+}
+)
+app.post("/quora/new",(req,res)=>{
+  
+    let{name,comment}=req.body;
+    let id=uuidv4();
+    console.log({name});
+    posts.push({id,name,comment});
+    for(post of posts){
+        console.log(post.name);
+    }
+   
+    res.redirect("/quora");
+})
+app.get("/quora/view/:id",(req,res)=>{
+    let {id}=req.params;
+    console.log(id);
+    let post=posts.find((p)=>id===p.id);
+    console.log(post.id);
+    res.render("view.ejs",{post});
+})
+app.get("/quora/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+})
+app.patch("/quora/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let newcomment=req.body.comment;
+
+    let post=posts.find((p)=>id===p.id);
+    post.comment=newcomment;
+    
+    res.redirect("/quora");
+})
